ac688943b55f5153101696fa8d5f122e6b575678
codemode9 <timourbasegmez@hotmail.com>
2018-03-08 22:01:13 -0500

    q

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
-    $(document.body).append(`<nav class="navbar navbar-light " style="background-color:#EB695E">
+    $(document.body).prepend(`<nav class="navbar navbar-light " style="background-color:#EB695E">
+    <div class="container">
+    <div class="col-sm-8">
     <a class="navbar-brand" style="color: white" href="#">
       Timour Basegmez
     </a>
-    <div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-secondary">About</button>
-  <button type="button" class="btn btn-secondary">Portfolio</button>
-  <button type="button" class="btn btn-secondary">Contact</button>
-</div>
-  </nav>`);
+    </div>
+    <div class="col-sm-4">
+  <button type="button" id="aboutBtn" class="btn btn-secondary">About me</button>
+  <button type="button" id="portBtn" class="btn btn-secondary">Portfolio</button>
+  <button type="button" id="contBtn" class="btn btn-secondary">Contact</button>
+  </div>
+  </div>
+  </nav>`
+
+);
 
 
 
