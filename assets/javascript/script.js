fda9555b48bcc11efe28449d94570c20e88332a6
codemode9 <timourbasegmez@hotmail.com>
2018-03-08 21:28:00 -0500

    new direction

---
+++ assets/javascript/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $(document.body).append(`<nav class="navbar navbar-light " style="background-color:#EB695E">
+    <a class="navbar-brand" style="color: white" href="#">
+      Timour Basegmez
+    </a>
+    <div class="btn-group" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-secondary">About</button>
+  <button type="button" class="btn btn-secondary">Portfolio</button>
+  <button type="button" class="btn btn-secondary">Contact</button>
+</div>
+  </nav>`);
+
+
+
+
+
+
+});
